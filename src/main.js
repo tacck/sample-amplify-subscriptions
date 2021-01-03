337ce41ed2429e9c6134b6d482633d15f5f1956f
Kihara, Takuya <gray@tacck.net>
2021-01-03 22:24:10 +0900

    amplify setup

--- src/main.js
+++ src/main.js
@@ -1,14 +1,20 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
-import vuetify from "./plugins/vuetify";
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import vuetify from './plugins/vuetify'
 
-Vue.config.productionTip = false;
+import Amplify from 'aws-amplify'
+import '@aws-amplify/ui-vue'
+import aws_exports from './aws-exports'
+
+Amplify.configure(aws_exports)
+
+Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
   vuetify,
-  render: h => h(App)
-}).$mount("#app");
+  render: h => h(App),
+}).$mount('#app')
